7dfda3e2b585178f796b8cd82359305d89ed2ddb
Dave Cross <dave@dave.org.uk>
2020-05-06 08:36:34 +0100

    Store the dbh in a session variable.

--- lib/Paste.pm
+++ lib/Paste.pm
@@ -5,7 +5,9 @@ our $VERSION = '0.1';
 
 use DBI;
 
-my $db = DBI->connect("dbi:SQLite:paste.db") || die "couldn't open";
+hook before => sub {
+  var dbh => DBI->connect("dbi:SQLite:paste.db") || die "couldn't open";
+};
 
 get '/' => sub {
   template 'request.tt', {
@@ -17,8 +19,12 @@ get '/' => sub {
 post '/paste' => sub {
   my $title = param('title');
   my $paste = param('paste');
-  $db->do('insert into pastes (title, paste) values (?, ?)', undef, $title, $paste);
-  my $key = $db->selectall_arrayref('select pastekey from pastes where paste = ?', undef, $paste);
+  vars->{dbh}->do(
+    'insert into pastes (title, paste) values (?, ?)', undef, $title, $paste
+  );
+  my $key = vars->{dbh}->selectall_arrayref(
+    'select pastekey from pastes where paste = ?', undef, $paste
+  );
   my $link = request->scheme . '://' . request->host . "/pastes/$key->[0][0]";
   template 'paste.tt', {
     'title' => $title,
@@ -30,8 +36,12 @@ post '/paste' => sub {
 
 get '/pastes/:id' => sub {
   my $id = params->{id};
-  my $title = $db->selectall_arrayref('select title from pastes where pastekey = ?', undef, $id);
-  my $paste = $db->selectall_arrayref('select paste from pastes where pastekey = ?', undef, $id);
+  my $title = vars->{dbh}->selectall_arrayref(
+    'select title from pastes where pastekey = ?', undef, $id
+  );
+  my $paste = vars->{dbh}->selectall_arrayref(
+    'select paste from pastes where pastekey = ?', undef, $id
+  );
   template 'paste.tt', {
     'title' => $title->[0][0],
     'paste' => $paste->[0][0],
